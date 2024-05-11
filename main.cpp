2020a12d0ec6e5cdae7a662348f37e3429d06e73
zhangts20 <1292276333@qq.com>
2024-05-11 14:57:04 +0800

    clean code

--- main.cpp
+++ main.cpp
@@ -121,19 +121,4 @@ int main() {
         }
         std::cout << std::endl;
     }
-
-
-    // std::ifstream t("/data/zhangtaoshan/models/llama2-7b/config.json");
-    // std::string t_str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
-
-    // rapidjson::Document document;
-    // document.Parse(t_str.c_str());
-
-    // std::cout << document.HasMember("model_type") << std::endl;
-    // std::cout << document["model_type"].GetString() << std::endl;
-    // if (document["architectures"].IsArray()) {
-    //     for (size_t i = 0; i < document["architectures"].Size(); ++i) {
-    //         std::cout << "arch: " << document["architectures"][i].GetString() << std::endl;
-    //     }
-    // }
 }
