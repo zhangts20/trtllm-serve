88059e24284df0de34bbd689ba28909dbc9bd116
zhangts20 <1292276333@qq.com>
2024-07-10 20:04:34 +0800

    add running pipeline

--- scripts/running_pipe.py
+++ scripts/running_pipe.py
@@ -38,8 +38,6 @@ class GenerationSession(object):
         self.vocab_size_padded = int(
             math.ceil(self.model_config.vocab_size / self.mapping.tp_size) *
             self.mapping.tp_size)
-
-        self.layer_types = ["attention"] * self.model_config.num_layers
         self.num_attn_layers = self.model_config.num_layers
 
     def _tensor_dtype(self, name):
@@ -126,7 +124,7 @@ class GenerationSession(object):
 
         self.use_one_more_block = False
 
-        self.buffer = dict()
+        self.buffer = {}
         # logits
         self.buffer["logits"] = torch.empty(
             (batch_size, self.vocab_size_padded),
@@ -182,7 +180,6 @@ class GenerationSession(object):
         input_ids: torch.Tensor,
         context_lengths: torch.Tensor,
         host_context_lengths: torch.Tensor,
-        max_context_length: int,
         position_ids: torch.Tensor,
         last_token_ids: torch.Tensor,
         attention_mask: torch.Tensor,
@@ -247,16 +244,16 @@ class GenerationSession(object):
                 host_request_types = torch.zeros_like(
                     host_context_lengths).int()
             else:
-                sequence_length = self.sequence_length_buffer
+                sequence_length = self.sequence_length_buffer + step
                 add_tensor_with_shape(sequence_length, "sequence_length",
                                       (batch_size, ))
                 host_past_key_value_lengths = torch.tensor(
-                    [max_context_length + step] * (batch_size, ),
+                    [self.max_context_length + step] * batch_size,
                     dtype=torch.int32)
                 add_tensor(host_past_key_value_lengths,
                            "host_past_key_value_lengths")
                 host_request_types = torch.ones_like(
-                    host_kv_cache_block_offsets).int()
+                    host_context_lengths).int()
             add_tensor(host_request_types, "host_request_types")
             add_tensor_with_shape(self.host_sink_token_length,
                                   "host_sink_token_length", (1, ))
@@ -265,139 +262,6 @@ class GenerationSession(object):
                                   (self.num_attn_layers, ))
             if self.removing_input_padding:
                 add_tensor(host_context_lengths, "host_context_lengths")
-
-        return tensors
-
-    def get_context_shape_buffer(
-        self,
-        input_ids: torch.Tensor,
-        context_lengths: torch.Tensor,
-        host_context_lengths: torch.Tensor,
-        position_ids: torch.Tensor,
-        last_token_ids: torch.Tensor,
-        attention_mask: torch.Tensor,
-        cache_indirection: torch.Tensor,
-        kv_cache_block_offsets: torch.Tensor,
-        host_kv_cache_block_offsets: torch.Tensor,
-    ) -> Dict[str, RuntimeTensor]:
-
-        def sym(x, name):
-            return RuntimeTensor.from_torch(name, x)
-
-        def add_tensor(x, name):
-            return tensors.update({name: sym(x, name)})
-
-        def add_tensor_with_shape(x, name, shape):
-            return tensors.update({
-                name:
-                RuntimeTensor.from_torch(name, x, override_shape=shape)
-            })
-
-        tensors = dict()
-        # gpt attention plugin
-        add_tensor(context_lengths, "context_lengths")
-        add_tensor(cache_indirection, "cache_indirection")
-        add_tensor(position_ids, "position_ids")
-        # assert attention_mask is None, "attention mask is None when using gpt attention plugin"
-
-        add_tensor(self.buffer["logits"], "logits")
-        add_tensor(last_token_ids, "last_token_ids")
-
-        add_tensor(input_ids, "input_ids")
-        # paged kv cache
-        buffer = kv_cache_block_offsets.contiguous()
-        shape = kv_cache_block_offsets.shape
-        shape = [shape[0] * shape[1], *shape[2:]]
-        add_tensor_with_shape(buffer, "kv_cache_block_offsets", shape)
-        add_tensor_with_shape(host_kv_cache_block_offsets,
-                              "host_kv_cache_block_offsets", shape)
-        add_tensor(self.buffer["host_kv_cache_pool_pointers"],
-                   "host_kv_cache_pool_pointers")
-
-        batch_size = context_lengths.shape[0]
-        host_request_types = torch.zeros_like(context_lengths,
-                                              device="cpu").int()
-        self.sequence_length_buffer = context_lengths.detach().clone()
-        add_tensor_with_shape(self.sequence_length_buffer, "sequence_length",
-                              (batch_size, ))
-        add_tensor_with_shape(host_context_lengths,
-                              "host_past_key_value_lengths", (batch_size, ))
-        add_tensor_with_shape(self.host_sink_token_length,
-                              "host_sink_token_length", (1, ))
-        add_tensor(host_request_types, "host_request_types")
-        add_tensor_with_shape(self.host_max_attention_window_sizes,
-                              "host_max_attention_window_sizes",
-                              (self.model_config.num_layers, ))
-        # remove input padding
-        add_tensor(host_context_lengths, "host_context_lengths")
-
-        return tensors
-
-    def get_next_step_shape_buffer(
-        self,
-        batch_size: int,
-        max_context_length: int,
-        step: int,
-        context_lengths: torch.Tensor,
-        host_context_lengths: torch.Tensor,
-        position_ids: torch.Tensor,
-        last_token_ids: torch.Tensor,
-        attention_mask: torch.Tensor,
-        cache_indirection: torch.Tensor,
-        kv_cache_block_offsets: torch.Tensor,
-        host_kv_cache_block_offsets: torch.Tensor,
-    ) -> Dict[str, RuntimeTensor]:
-
-        def sym(x, name):
-            return RuntimeTensor.from_torch(name, x)
-
-        def add_tensor(x, name):
-            return tensors.update({name: sym(x, name)})
-
-        def add_tensor_with_shape(x, name, shape):
-            return tensors.update({
-                name:
-                RuntimeTensor.from_torch(name, x, override_shape=shape)
-            })
-
-        tensors = dict()
-        # use gpt attention
-        add_tensor(context_lengths, "context_lengths")
-        add_tensor(cache_indirection, "cache_indirection")
-        add_tensor(position_ids, "position_ids")
-        # assert attention_mask is None, "attention mask is None when using gpt attention plugin"
-
-        add_tensor(self.buffer["logits"], "logits")
-        add_tensor(last_token_ids, "last_token_ids")
-
-        add_tensor_with_shape(self.new_tokens, "input_ids", (batch_size, ))
-        # paged kv cache
-        shape = kv_cache_block_offsets.shape
-        shape = [shape[0] * shape[1], *shape[2:]]
-        add_tensor_with_shape(kv_cache_block_offsets, "kv_cache_block_offsets",
-                              shape)
-        add_tensor_with_shape(host_kv_cache_block_offsets,
-                              "host_kv_cache_block_offsets", shape)
-        add_tensor(self.buffer["host_kv_cache_pool_pointers"],
-                   "host_kv_cache_pool_pointers")
-        # use gpt attention plugin
-        host_request_types = torch.ones_like(context_lengths,
-                                             device="cpu").int()
-        host_past_key_value_lengths = torch.tensor(
-            [max_context_length + step] * batch_size,
-            dtype=torch.int32,
-            device="cpu")
-        add_tensor(host_past_key_value_lengths, "host_past_key_value_lengths")
-        add_tensor(host_request_types, "host_request_types")
-        sequence_length = self.sequence_length_buffer + step
-        add_tensor_with_shape(sequence_length, "sequence_length",
-                              (batch_size, ))
-        add_tensor_with_shape(self.host_sink_token_length,
-                              "host_sink_token_length", (1, ))
-        add_tensor_with_shape(self.host_max_attention_window_sizes,
-                              "host_max_attention_window_sizes",
-                              (self.model_config.num_layers, ))
-        add_tensor(host_context_lengths, "host_context_lengths")
 
         return tensors
 
@@ -410,19 +274,14 @@ class GenerationSession(object):
         batch_size: int,
         context_lengths: torch.Tensor,
         host_context_lengths: torch.Tensor,
-        max_context_length: int,
-        cache_indirections: List[torch.Tensor],
-        input_ids: torch.Tensor,
+        cache_indirection: List[torch.Tensor],
+        batch_input_ids: torch.Tensor,
     ) -> torch.Tensor:
         for step in range(self.max_new_tokens):
             if step % 2:
                 context = self.runtime.context_0
-                curr_cache_indirection = cache_indirections[1]
-                next_cache_indirection = cache_indirections[0]
             else:
                 context = self.runtime.context_1
-                curr_cache_indirection = cache_indirections[0]
-                next_cache_indirection = cache_indirections[1]
             # prefill
             if step == 0:
                 model_inputs = self._prepare_context_inputs(
@@ -440,26 +299,19 @@ class GenerationSession(object):
                 batch_size = context_lengths.size(0)
                 ctx_tensors = self.get_shape_buffer(
                     batch_size=batch_size,
-                    input_ids=input_ids,
+                    input_ids=batch_input_ids,
                     context_lengths=context_lengths,
                     host_context_lengths=host_context_lengths,
-                    max_context_length=max_context_length,
                     position_ids=position_ids,
                     last_token_ids=last_token_ids,
                     attention_mask=attention_mask,
-                    cache_indirection=curr_cache_indirection,
+                    cache_indirection=cache_indirection,
                     kv_cache_block_offsets=kv_cache_block_offsets,
                     host_kv_cache_block_offsets=host_kv_cache_block_offsets,
                     step=step,
                     is_prefill=True)
 
-                # ctx_tensors = self.get_context_shape_buffer(
-                #     input_ids, context_lengths, host_context_lengths,
-                #     position_ids, last_token_ids, attention_mask,
-                #     curr_cache_indirection, kv_cache_block_offsets,
-                #     host_kv_cache_block_offsets)
                 context = self.runtime.ctx_context
-                import pdb; pdb.set_trace()
                 self.runtime._set_tensors(context, ctx_tensors)
 
             stream = torch.cuda.current_stream().cuda_stream
@@ -479,43 +331,39 @@ class GenerationSession(object):
                     1)
                 kv_cache_block_offsets = host_kv_cache_block_offsets.to("cuda")
 
-                self.new_tokens = torch.tensor([[0]],
-                                               device=self.device,
-                                               dtype=torch.int32)
+                next_token_ids = self.get_next_tokens(self.buffer["logits"])
                 next_context = self.runtime.context_1 if step % 2 else self.runtime.context_0
-                next_step_tensors = self.get_next_step_shape_buffer(
-                    batch_size, max_context_length, step, context_lengths,
-                    host_context_lengths, position_ids, last_token_ids,
-                    attention_mask, next_cache_indirection,
-                    kv_cache_block_offsets, host_kv_cache_block_offsets)
+                next_step_tensors = self.get_shape_buffer(
+                    batch_size=batch_size,
+                    input_ids=next_token_ids,
+                    context_lengths=context_lengths,
+                    host_context_lengths=host_context_lengths,
+                    position_ids=position_ids,
+                    last_token_ids=last_token_ids,
+                    attention_mask=attention_mask,
+                    cache_indirection=cache_indirection,
+                    kv_cache_block_offsets=kv_cache_block_offsets,
+                    host_kv_cache_block_offsets=host_kv_cache_block_offsets,
+                    step=step,
+                    is_prefill=False)
                 print(">>>>>> step=", step)
                 for k, v in next_step_tensors.items():
                     print(f"{k}={v.to_torch()}")
                     print(f"{k}.shape={v.to_torch().shape}")
                 self.runtime._set_tensors(next_context, next_step_tensors)
 
-            # update input ids
-            self.new_tokens = self.get_next_tokens(self.buffer["logits"])
-
     def decode(
         self,
+        batch_size: int,
         batch_input_ids: List[torch.Tensor],
-        input_lengths: torch.Tensor,
+        context_lengths: torch.Tensor,
     ) -> torch.Tensor:
-        batch_size = input_lengths.size(0)
-        max_context_length = torch.max(input_lengths).item()
-        host_context_length = input_lengths.cpu()
-
-        cache_indirections = [
-            torch.full((batch_size, 1, self.max_attention_window_size),
-                       0,
-                       dtype=torch.int32,
-                       device=self.device),
-            torch.full((batch_size, 1, self.max_attention_window_size),
-                       0,
-                       dtype=torch.int32,
-                       device=self.device),
-        ]
+        host_context_length = context_lengths.cpu()
+        cache_indirection = torch.full(
+            (batch_size, 1, self.max_attention_window_size),
+            0,
+            dtype=torch.int32,
+            device=self.device)
         # paged kv cache
         num_blocks, max_blocks_per_seq = self._get_num_paged_blocks(
             self.max_attention_window_size, self.sink_token_length,
@@ -539,11 +387,13 @@ class GenerationSession(object):
         for b in range(batch_size):
             generation_seq = GenerationSequence(b, b)
             self.kv_cache_manager.add_sequence(generation_seq,
-                                               max_context_length)
+                                               self.max_context_length)
 
-        return self.decode_regular(batch_size, input_lengths,
-                                   host_context_length, max_context_length,
-                                   cache_indirections, batch_input_ids)
+        return self.decode_regular(batch_size=batch_size,
+                                   context_lengths=context_lengths,
+                                   host_context_lengths=host_context_length,
+                                   cache_indirection=cache_indirection,
+                                   batch_input_ids=batch_input_ids)
 
 
 class EngineConfig(object):
@@ -608,18 +458,19 @@ class ModelRunner(object):
 
         return batch_input_ids, input_lengths
 
-    def generate(self, batch_input_ids: torch.Tensor):
+    def generate(self, batch_input_ids: torch.Tensor, max_new_tokens: int):
+        # prepare inputs
         batch_size = len(batch_input_ids)
         batch_input_ids, input_lengths = self.prepare_inputs(batch_input_ids)
-
+        # setup env for generation
         self.session.setup(batch_size=batch_size,
                            max_context_length=input_lengths.max().item(),
-                           max_new_tokens=17,
+                           max_new_tokens=max_new_tokens,
                            beam_width=1)
         batch_input_ids = batch_input_ids.cuda()
         input_lengths = input_lengths.cuda()
-
-        return self.session.decode(batch_input_ids, input_lengths)
+        # generation
+        return self.session.decode(batch_size, batch_input_ids, input_lengths)
 
     @classmethod
     def from_engine(cls, engine: Engine, rank: int) -> "ModelRunner":
@@ -738,5 +589,5 @@ if __name__ == "__main__":
     runner = ModelRunner.from_dir(args.engine_dir, rank=runtime_rank)
     # 3. generate
     with torch.no_grad():
-        runner.generate(batch_input_ids)
-        torch.cuda.synchronize()
+        runner.generate(batch_input_ids=batch_input_ids,
+                        max_new_tokens=args.max_new_tokens)
